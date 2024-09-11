2595ad3a7b7544c165f023f927581a837ede9038
CodewithSanthu <santhanalakshmi.ramalingam12@gmail.com>
2024-09-11 12:04:04 +0530

    Initial commit

---
+++ src/Expense.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+public class Expense {
+    private Date date;
+    private String category;
+    private double amount;
+
+    public Expense(Date date, String category, double amount) {
+        this.date = date;
+        this.category = category;
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
